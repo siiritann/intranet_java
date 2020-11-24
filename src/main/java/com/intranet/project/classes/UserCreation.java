c1857201c00c2b1ed1b71e76e38f012a688a0e3d
SaskiaR <saskia.rohtla@gmail.com>
2020-11-24 11:47:06 +0200

    password to UserCreation

--- src/main/java/com/intranet/project/classes/UserCreation.java
+++ src/main/java/com/intranet/project/classes/UserCreation.java
@@ -6,9 +6,18 @@ import java.util.Date;
 public class UserCreation {
     // User Response class for User creation
     private String username;
+    private String password;
     // isAdmin is by default false, each user can be set to admin by the first admin
     private boolean isAdmin = false;
     private String email;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
