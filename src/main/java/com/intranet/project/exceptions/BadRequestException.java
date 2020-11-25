ae1b78aa137c049d3a45fee78a1365f1eecaa978
Ott Kask <ott.kask@gmail.com>
2020-11-25 11:06:49 +0200

    3 exceptions

---
+++ src/main/java/com/intranet/project/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.intranet.project.exceptions;
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/intranet/project/exceptions/IntranetExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.intranet.project.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class IntranetExceptionHandler extends ResponseEntityExceptionHandler{
+    @ExceptionHandler(InternalServerErrorException.class)
+    public ResponseEntity<Object> handleInternalServerErrorException(Exception e){
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage()),
+                new HttpHeaders(),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Object> handleBadRequestException(Exception e){
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage()),
+                new HttpHeaders(),
+                HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleNotFoundException(Exception e){
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage()),
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND);
+    }
+}
